f1d2a0c8a7ab24844cd06c367a3ab5d450b0e9b5
Jordan Vidrine <jordan@jordanvidrine.com>
2020-09-01 16:30:36 -0500

    Dark Light Toggle Additions

--- javascripts/discourse/initializers/dark-light-toggle.js
+++ javascripts/discourse/initializers/dark-light-toggle.js
@@ -6,6 +6,73 @@ export default {
 
   initialize() {
 
+    let lightTheme = document.getElementsByClassName("light-scheme")[0];
+    let darkTheme = document.getElementsByClassName("dark-scheme")[0];
+
+    let switchToDark = function() {
+      lightTheme.media = "none";
+      lightTheme.classList.remove("user-selected-theme");
+      darkTheme.media = "all";
+      darkTheme.classList.add("user-selected-theme");
+      localStorage.setItem('userToggledScheme', 'dark')
+    };
+
+    let switchToLight = function() {
+      darkTheme.media = "none";
+      darkTheme.classList.remove("user-selected-theme");
+      lightTheme.media = "all";
+      lightTheme.classList.add("user-selected-theme");
+      localStorage.setItem('userToggledScheme', 'light')
+    };
+
+    let loadDarkOrLight = function() {
+      let savedSchemeChoice = localStorage.getItem("userToggledScheme");
+      
+      if (savedSchemeChoice === "light") {
+        darkTheme.media = "none";
+        darkTheme.classList.remove("user-selected-theme");
+        lightTheme.media = "all";
+        lightTheme.classList.add("user-selected-theme");
+      } else if (savedSchemeChoice === "dark") {
+        lightTheme.media = "none";
+        lightTheme.classList.remove("user-selected-theme");
+        darkTheme.media = "all";
+        darkTheme.classList.add("user-selected-theme");
+      } else {
+        return;
+      }
+    }
+
+    loadDarkOrLight();
+
+    let toggleDL = function() { 
+      let page = document.getElementsByTagName("html")[0];
+      let style = window
+        .getComputedStyle(page)
+        .getPropertyValue("--scheme-type")
+        .trim();     
+
+      if (style === "light") {
+        switchToDark();
+      } else {
+        switchToLight();
+      }
+  
+      let darkLightToggle = document.getElementsByClassName(
+        "dark-light-toggle"
+      )[0].children[0].children[0];
+
+      // flip 'adjust' icon
+      if (
+        darkLightToggle.style.transform === "scale(1)" ||
+        darkLightToggle.style.transform === ""
+      ) {
+        darkLightToggle.style.transform = "scale(-1,1)";
+      } else {
+        darkLightToggle.style.transform = "scale(1)";
+      }
+    };
+
     withPluginApi("0.8", api => {
       api.addQuickAccessProfileItem({
         icon: "adjust",
@@ -15,43 +82,9 @@ export default {
       })
 
       api.reopenWidget("user-menu", {
+
         toggleDarkLight() {
-          let lightTheme = document.getElementsByClassName("light-scheme")[0];
-          let darkTheme = document.getElementsByClassName("dark-scheme")[0];
-      
-          let page = document.getElementsByTagName("html")[0];
-      
-          let style = window
-            .getComputedStyle(page)
-            .getPropertyValue("--scheme-type")
-            .trim();
-
-          // if light mode
-          if (style === "light") {
-            lightTheme.media = "none";
-            lightTheme.classList.remove("user-selected-theme");
-            darkTheme.media = "all";
-            darkTheme.classList.add("user-selected-theme");
-            // If dark mode
-          } else {
-            darkTheme.media = "none";
-            darkTheme.classList.remove("user-selected-theme");
-            lightTheme.media = "all";
-            lightTheme.classList.add("user-selected-theme");
-          }
-      
-          let darkLightToggle = document.getElementsByClassName(
-            "dark-light-toggle"
-          )[0].children[0].children[0];
-
-          if (
-            darkLightToggle.style.transform === "scale(1)" ||
-            darkLightToggle.style.transform === ""
-          ) {
-            darkLightToggle.style.transform = "scale(-1,1)";
-          } else {
-            darkLightToggle.style.transform = "scale(1)";
-          }
+          toggleDL();
         }
       })
     });
