d151cdbcc773404705fed954b70f3b5dbd3b92dc
Jordan Vidrine <jordan@jordanvidrine.com>
2020-09-03 12:06:11 -0500

    FIX: Add js file to repo

---
+++ javascripts/discourse/initializers/dark-light-toggle-hamburger.js
@@ -0,0 +1,120 @@
+import { withPluginApi } from "discourse/lib/plugin-api";
+import I18n from "I18n";
+import { h } from "virtual-dom";
+
+export default {
+  name: "dark-light-toggle-hamburger-initializer",
+
+  initialize() {
+    // get the two <link> elements that hold the dark/light variables
+    let lightTheme = document.querySelector(".light-scheme");
+    let darkTheme = document.querySelector(".dark-scheme");
+
+    let switchToDark = function () {
+      lightTheme.media = "none";
+      lightTheme.classList.remove("user-selected-theme");
+      darkTheme.media = "all";
+      darkTheme.classList.add("user-selected-theme");
+      localStorage.setItem("userToggledScheme", "dark");
+    };
+
+    let switchToLight = function () {
+      darkTheme.media = "none";
+      darkTheme.classList.remove("user-selected-theme");
+      lightTheme.media = "all";
+      lightTheme.classList.add("user-selected-theme");
+      localStorage.setItem("userToggledScheme", "light");
+    };
+
+    let toggleDarkLight = function () {
+      let page = document.getElementsByTagName("html")[0];
+      let style = window
+        .getComputedStyle(page)
+        .getPropertyValue("--scheme-type")
+        .trim();
+
+      if (style === "light") {
+        switchToDark();
+      } else {
+        switchToLight();
+      }
+    };
+
+    // loads user setting from local storage if there is one upon init
+    let loadDarkOrLight = function () {
+      let savedSchemeChoice = localStorage.getItem("userToggledScheme");
+
+      if (savedSchemeChoice === "light") {
+        darkTheme.media = "none";
+        darkTheme.classList.remove("user-selected-theme");
+        lightTheme.media = "all";
+        lightTheme.classList.add("user-selected-theme");
+      } else if (savedSchemeChoice === "dark") {
+        lightTheme.media = "none";
+        lightTheme.classList.remove("user-selected-theme");
+        darkTheme.media = "all";
+        darkTheme.classList.add("user-selected-theme");
+      } else {
+        return;
+      }
+    };
+
+    loadDarkOrLight();
+
+    withPluginApi("0.8", api => {
+      api.createWidget("scheme-selector", {
+        buildKey: attrs => "scheme-selector",
+
+        defaultState() {
+          let page = document.getElementsByTagName("html")[0];
+          let style = window
+            .getComputedStyle(page)
+            .getPropertyValue("--scheme-type")
+            .trim();
+
+          return { currentScheme: style };
+        },
+
+        click() {
+          toggleDarkLight();
+
+          let page = document.getElementsByTagName("html")[0];
+          let style = window
+            .getComputedStyle(page)
+            .getPropertyValue("--scheme-type")
+            .trim();
+
+          let toggleText = document.querySelector(".dark-light-toggle");
+          toggleText.textContent =
+            style === "light"
+              ? I18n.t(themePrefix("toggle_dark_mode"))
+              : I18n.t(themePrefix("toggle_light_mode"));
+        },
+
+        schemeSelectorHtml(currentScheme) {
+          let schemeName =
+            currentScheme === "light" ? "Dark Mode" : "Light Mode";
+          return h(
+            "li",
+            h("a.widget-link.dark-light-toggle", `Toggle ${schemeName}`)
+          );
+        },
+
+        html(attrs, state) {
+          let schemeSelectorHtml = this.schemeSelectorHtml(state.currentScheme);
+          return [
+            h("ul.menu-links.columned", schemeSelectorHtml),
+            h(".clearfix"),
+            h("hr")
+          ];
+        }
+      });
+
+      api.decorateWidget("menu-links:before", helper => {
+        if (helper.attrs.name === "footer-links") {
+          return [helper.widget.attach("scheme-selector")];
+        }
+      });
+    });
+  }
+};
