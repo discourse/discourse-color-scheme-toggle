a4f11157fbe1e211a7f77ea4dba53b76b3c360c7
Jordan Vidrine <jordan@jordanvidrine.com>
2020-09-21 16:25:17 -0500

    Refactor + Add cookie functionality

--- javascripts/discourse/initializers/dark-light-toggle-hamburger.js
+++ javascripts/discourse/initializers/dark-light-toggle-hamburger.js
@@ -2,6 +2,7 @@ import { withPluginApi } from "discourse/lib/plugin-api";
 import I18n from "I18n";
 import { h } from "virtual-dom";
 import { iconNode } from "discourse-common/lib/icon-library";
+import cookie from "discourse/lib/cookie";
 
 export default {
   name: "dark-light-toggle-hamburger-initializer",
@@ -16,6 +17,7 @@ export default {
     }
 
     let switchToDark = function () {
+      cookie("userSelectedScheme","dark");
       lightTheme.media = "none";
       lightTheme.classList.remove("user-selected-theme");
       darkTheme.media = "all";
@@ -23,6 +25,7 @@ export default {
     };
 
     let switchToLight = function () {
+      cookie("userSelectedScheme","light");
       darkTheme.media = "none";
       darkTheme.classList.remove("user-selected-theme");
       lightTheme.media = "all";
@@ -30,45 +33,100 @@ export default {
     };
 
     let switchToAuto = function () {
+      cookie("userSelectedScheme","auto");
       darkTheme.media = "(prefers-color-scheme: dark)";
       darkTheme.classList.remove("user-selected-theme");
       lightTheme.media = "all";
       lightTheme.classList.remove("user-selected-theme");
     };
 
+    let toggleDarkLight = function () {
+      let page = document.getElementsByTagName("html")[0];
+      let style = window
+        .getComputedStyle(page)
+        .getPropertyValue("--scheme-type")
+        .trim();
+      
+      if (style === "light") {
+        switchToDark();
+      } else {
+        switchToLight();
+      }
+    };
+
+    let loadDarkOrLight = function () {
+      
+      let savedSchemeChoice = cookie("userSelectedScheme");
+
+      if (savedSchemeChoice === "light") {
+        darkTheme.media = "none";
+        darkTheme.classList.remove("user-selected-theme");
+        lightTheme.media = "all";
+        lightTheme.classList.add("user-selected-theme");
+      } else if (savedSchemeChoice === "dark") {
+        lightTheme.media = "none";
+        lightTheme.classList.remove("user-selected-theme");
+        darkTheme.media = "all";
+        darkTheme.classList.add("user-selected-theme");
+      } else if (savedSchemeChoice === "auto") {
+        lightTheme.media = "all";
+        lightTheme.classList.remove("user-selected-theme");
+        darkTheme.media = "(prefers-color-scheme: dark)";
+        darkTheme.classList.remove("user-selected-theme");
+      }
+    };
+
+    loadDarkOrLight();
 
     withPluginApi("0.8", api => {
-      api.createWidget("dark-selector", {
-        buildKey: attrs => "dark-selector",
+      api.createWidget("dark-light-selector", {
+        buildKey: attrs => "dark-light-selector",
 
-        click() {
-          switchToDark();
+        defaultState() {
+          let page = document.getElementsByTagName("html")[0];
+          let style = window
+            .getComputedStyle(page)
+            .getPropertyValue("--scheme-type")
+            .trim();
+
+          return { currentScheme: style };
         },
 
-        html() {
-          return h("a.widget-link.dark-light-toggle",[
-            iconNode("far-moon", {
-              class: "show-scheme-icon"
-            }),
-            h("p", I18n.t(themePrefix("toggle_dark_mode")))
-            ]
-          );
-        }
-      });
-
-      api.createWidget("light-selector", {
-        buildKey: attrs => "light-selector",
-
         click() {
-          switchToLight();
+          toggleDarkLight();
+
+          let page = document.getElementsByTagName("html")[0];
+          let style = window
+            .getComputedStyle(page)
+            .getPropertyValue("--scheme-type")
+            .trim();
+
+          let toggleText = document.querySelector(".dark-light-toggle").children[2];
+          toggleText.textContent =
+            style === "light"
+              ? I18n.t(themePrefix("toggle_dark_mode"))
+              : I18n.t(themePrefix("toggle_light_mode"));
+          
+          let schemeIcons = Array.prototype.slice.call(document.querySelectorAll('.scheme-icon'));
+
+          schemeIcons.forEach((icon) => {
+            icon.classList.toggle('show-scheme-icon')
+          })
+
         },
 
-        html() {
+        html(attrs, state) {
+          let schemeName =
+            state.currentScheme === "light" ? "toggle_dark_mode" : "toggle_light_mode";
+
           return h("a.widget-link.dark-light-toggle",[
             iconNode("far-sun", {
-              class: "show-scheme-icon"
+              class: state.currentScheme === "dark" ? "scheme-icon show-scheme-icon" : "scheme-icon"
             }),
-            h("p", I18n.t(themePrefix("toggle_light_mode")))
+            iconNode("far-moon", {
+              class: state.currentScheme === "light" ? "scheme-icon show-scheme-icon" : "scheme-icon"
+            }),
+            h("p", I18n.t(themePrefix(schemeName)))
             ]
           );
         }
@@ -79,6 +137,19 @@ export default {
 
         click() {
           switchToAuto();
+
+          let page = document.getElementsByTagName("html")[0];
+          let style = window
+            .getComputedStyle(page)
+            .getPropertyValue("--scheme-type")
+            .trim();
+
+          let toggleText = document.querySelector(".dark-light-toggle").children[2];
+          toggleText.textContent =
+            style === "light"
+              ? I18n.t(themePrefix("toggle_dark_mode"))
+              : I18n.t(themePrefix("toggle_light_mode"));
+
         },
 
         html() {
@@ -96,8 +167,7 @@ export default {
         if (helper.attrs.name === "footer-links") {
           return [
             h("ul.color-scheme-toggle", [
-              h("li",helper.widget.attach("dark-selector")),
-              h("li",helper.widget.attach("light-selector")),
+              h("li",helper.widget.attach("dark-light-selector")),
               h("li",helper.widget.attach("auto-selector"))
             ]),
             h(".clearfix"),
