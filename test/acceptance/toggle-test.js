discourse/discourse-color-scheme-toggle

commit c9b18391a2e8dca6c7c8c8d9b7f8ec5be153752a

FIX: Load correct override if necessary (#25)
And don't apply alternative stylesheet by default if user hasn't opted
for it.

javascripts/discourse/connectors/sidebar-footer-actions/toggler-button.js
@@ -5,6 +5,9 @@ export default {
     component.showInSidebar = false;
 
     if (!Session.currentProp("darkModeAvailable")) {
+      if (component.siteSettings.default_dark_mode_color_scheme_id > 0) {
+        component.showInSidebar = true;
+      }
       return;
     }
 

.discourse-compatibility
@@ -1,0 +1,1 @@
+3.1.0.beta3: 8be5bef97767262d1a727773f6aafc0addab9b0c

javascripts/discourse/initializers/color-scheme-toggler.js
@@ -12,14 +12,26 @@ export default {
   name: "color-scheme-toggler",
 
   initialize(container) {
+    const keyValueStore = container.lookup("service:key-value-store");
+    const storedOverride = keyValueStore.getItem(COLOR_SCHEME_OVERRIDE_KEY);
+
     if (!Session.currentProp("darkModeAvailable")) {
       const siteSettings = container.lookup("site-settings:main");
+
       if (siteSettings.default_dark_mode_color_scheme_id > 0) {
         loadColorSchemeStylesheet(
           siteSettings.default_dark_mode_color_scheme_id,
           currentThemeId(),
           true
-        );
+        ).then(() => {
+          if (storedOverride) {
+            colorSchemeOverride(storedOverride);
+          } else {
+            // ensures that this extra stylesheet isn't auto-used when OS is in dark mode
+            document.querySelector("link#cs-preview-dark").media =
+              "(prefers-color-scheme: none)";
+          }
+        });
       } else {
         // eslint-disable-next-line no-console
         console.warn(
@@ -29,8 +41,6 @@ export default {
       }
     }
 
-    const keyValueStore = container.lookup("service:key-value-store");
-    const storedOverride = keyValueStore.getItem(COLOR_SCHEME_OVERRIDE_KEY);
     if (storedOverride) {
       Session.currentProp("colorSchemeOverride", storedOverride);
     }

commit 06f9c978a84ea3b37e75bfe39556b9618d1beba2

FIX: Don't show button in both sidebar and header
Fixes a regression introduced in https://github.com/discourse/discourse-color-scheme-toggle/pull/25

javascripts/discourse/connectors/sidebar-footer-actions/toggler-button.js
@@ -4,10 +4,10 @@ export default {
   setupComponent(_args, component) {
     component.showInSidebar = false;
 
-    if (!Session.currentProp("darkModeAvailable")) {
-      if (component.siteSettings.default_dark_mode_color_scheme_id > 0) {
-        component.showInSidebar = true;
-      }
+    if (
+      !Session.currentProp("darkModeAvailable") &&
+      component.siteSettings.default_dark_mode_color_scheme_id < 0
+    ) {
       return;
     }
 

test/acceptance/toggle-test.js
@@ -65,3 +65,37 @@ acceptance("Color Scheme Toggle - sidebar icon", function (needs) {
     assert.ok(visible(toggleButton), "button in footer");
   });
 });
+
+acceptance("Color Scheme Toggle - sidebar icon", function (needs) {
+  needs.pretender((server, helper) => {
+    server.get("/color-scheme-stylesheet/2.json", () => {
+      return helper.response({
+        color_scheme_id: 2,
+        new_href:
+          "/stylesheets/color_definitions_wcag-dark_2_52_a09af66a69bc639c6d63acd81d4c3cea2985282e.css",
+      });
+    });
+  });
+
+  needs.settings({
+    enable_sidebar: true,
+    enable_experimental_sidebar_hamburger: true,
+    default_dark_mode_color_scheme_id: 2,
+  });
+
+  needs.hooks.beforeEach(() => {
+    settings.add_color_scheme_toggle_to_header = false;
+  });
+
+  test("shows in sidebar if site has auto dark mode", async function (assert) {
+    await visit("/");
+
+    assert.ok(
+      !visible(".header-color-scheme-toggle"),
+      "button not present in header"
+    );
+
+    const toggleButton = ".sidebar-footer-wrapper .color-scheme-toggler";
+    assert.ok(visible(toggleButton), "button in footer");
+  });
+});